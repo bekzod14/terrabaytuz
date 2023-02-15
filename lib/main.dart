015251fb851b7981fe3b011797d3c297778b3bf6
pokizaSaparmatova <106673440+pokizaSaparmatova@users.noreply.github.com>
2023-02-15 14:30:09 +0500

    di qismda setUp funtion yozildi va nain screen da chaqirib qo'yildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:terrabayt_uz/pages/home_page.dart';
 
+import 'di/di_module.dart';
+
 void main() {
+  setUp();
   runApp(const MaterialApp(
+
     home: HomePage(),
   ));
 }
