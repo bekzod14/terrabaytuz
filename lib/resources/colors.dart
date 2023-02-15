7d13edb4323a2ce85b90fef305578f61884ebf80
macbook <bekzodbosimbekov1403@gmail.com>
2023-02-15 17:29:56 +0500

    add colors to resources

---
+++ lib/resources/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFFFF3A44);
+  static const Color secondary = Color(0xFF0080FF);
+  static const Color tertiary = Color(0xFFFFE600);
+}
