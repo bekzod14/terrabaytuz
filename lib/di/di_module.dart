26884a0d1b02d6bd1fc517bed81bb33f49499a2a
pokizaSaparmatova <106673440+pokizaSaparmatova@users.noreply.github.com>
2023-02-15 14:10:49 +0500

    di qismda setUp funtion yozildi va nain screen da chaqirib qo'yildi

--- lib/di/di_module.dart
+++ lib/di/di_module.dart
@@ -1,7 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:terrabayt_uz/api/news_api.dart';
 
 final di = GetIt.instance;
 
 void setUp(){
+  di.registerLazySingleton(() => Dio(BaseOptions(baseUrl:"")));
+  di.registerLazySingleton(() => NewsApi(di.get<Dio>()));
 
 }
