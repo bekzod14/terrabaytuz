9cfb88538ef19050dcba1d73a2f13de0db0c6d0c
pokizaSaparmatova <106673440+pokizaSaparmatova@users.noreply.github.com>
2023-02-15 14:59:46 +0500

    api

--- lib/di/di_module.dart
+++ lib/di/di_module.dart
@@ -1,9 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import '../api/news_api.dart';
 
 final di = GetIt.instance;
 
-void setUp(){
-  di.registerLazySingleton(() => Dio(BaseOptions(baseUrl:"")));
+void setUp() {
+  di.registerLazySingleton(() => Dio(BaseOptions(baseUrl: "https://www.terabayt.uz/")));
   di.registerLazySingleton(() => NewsApi(di.get<Dio>()));
-
 }
