d4b6f0666db2dfe7a9b75cfe5a2bd5b7633bb670
pokizaSaparmatova <106673440+pokizaSaparmatova@users.noreply.github.com>
2023-02-15 15:15:23 +0500

    post api

--- lib/api/news_api.dart
+++ lib/api/news_api.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:terrabayt_uz/data/models/category_data.dart';
+import 'package:terrabayt_uz/data/models/news_data.dart';
 
 class NewsApi {
   final Dio _dio;
@@ -7,10 +8,15 @@ class NewsApi {
   NewsApi(this._dio);
 
   Future<List<CategoryData>> getCategories() async {
-    final response =
-        await _dio.get("https://www.terabayt.uz/api.php?action=categories");
+    final response = await _dio.get("api.php?action=categories");
     print("response: $response");
     print("response: ${response.data}");
     return categoryDataFromMap(response.data as String);
+  }
+
+  Future<List<NewsData>> getPost(int categoryId) async {
+    final respose = await _dio.get(
+        "api.php?action=posts&first_update=1613122152&last_update=0&category=$categoryId");
+    return newsDataFromMap(respose.data as String);
   }
 }
